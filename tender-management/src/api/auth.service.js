c492f1f59b210f7bf329c1b3ca0c5dab5bd972c4
Karishma Khatri <114894267+KNkarishma2000@users.noreply.github.com>
2026-01-24 17:52:27 +0530

    Update auth.service.js

--- tender-management/src/api/auth.service.js
+++ tender-management/src/api/auth.service.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5000/api'; // Change to your production URL later
+const API_BASE_URL = 'https://palegreen-rhinoceros-358698.hostingersite.com/api'; // Change to your production URL later
 
 // Create an instance with default config
 const apiClient = axios.create({
@@ -307,6 +307,7 @@ export const financeAPI = {
     apiClient.get('/drive/zoho-elemensor-history'), // Matches new rout
 };
 export default apiClient;
+
 
 
 
