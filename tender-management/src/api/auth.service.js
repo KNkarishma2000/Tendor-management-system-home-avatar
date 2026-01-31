7346a138a30e1350faeaf9c09adccd31edfaa8f3
Karishma Khatri <114894267+KNkarishma2000@users.noreply.github.com>
2026-01-31 15:40:16 +0530

    Update auth.service.js

--- tender-management/src/api/auth.service.js
+++ tender-management/src/api/auth.service.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://palegreen-rhinoceros-358698.hostingersite.com/api'; // Change to your production URL later
+const API_BASE_URL = 'https://palegreen-rhinoceros-358698.hostingersite.com/api'; // Change to your production URL later
 
 // Create an instance with default config
 const apiClient = axios.create({
@@ -323,6 +323,7 @@ export const supplierAPI = {
   }),
 };
 export default apiClient;
+
 
 
 
